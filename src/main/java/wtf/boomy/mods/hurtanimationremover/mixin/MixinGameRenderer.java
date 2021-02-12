14411d5be89b85e8578842eba88e9484a7220057
boomboompower <boomboompowermc@gmail.com>
2021-02-12 20:57:57 +1100

    make cancellable

--- src/main/java/wtf/boomy/mods/hurtanimationremover/mixin/MixinGameRenderer.java
+++ src/main/java/wtf/boomy/mods/hurtanimationremover/mixin/MixinGameRenderer.java
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
-public class MixinEntityRenderer {
+public class MixinGameRenderer {
 
-    @Inject(method = "bobViewWhenHurt", at = @At("HEAD"))
+    @Inject(method = "bobViewWhenHurt", at = @At("HEAD"), cancellable = true)
     private void injectBobViewWhenHurt(MatrixStack matrixStack, float f, CallbackInfo ci) {
         ci.cancel();
     }
